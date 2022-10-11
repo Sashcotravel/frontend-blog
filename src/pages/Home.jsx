c8113b906fc8785cfc2d8675d0ab4459ba87d426
Sashcotravel <93523518+Sashcotravel@users.noreply.github.com>
2022-10-11 12:40:18 +0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -33,23 +33,22 @@ export const Home = () => {
             </Tabs>
             <Grid container spacing={4}>
                 <Grid xs={8} item>
-//                     {/*{(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) => (*/}
-                    {/*    isPostsLoading ? (<Post key={index} isLoading={true}/>)*/}
-                    {/*        : (*/}
-                    {/*        <Post*/}
-                    {/*            key={index}*/}
-                    {/*            _id={obj._id}*/}
-                    {/*            title={obj.title}*/}
-                    {/*            // http://localhost:4444*/}
-                    {/*            imageUrl={obj.imageUrl ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}` : ''}*/}
-                    {/*            user={obj.user}*/}
-                    {/*            createdAt={obj.createdAt}*/}
-                    {/*            viewsCount={obj.viewsCount}*/}
-                    {/*            commentsCount={3}*/}
-                    {/*            tags={obj.tags}*/}
-                    {/*            isEditable={userData?._id === obj.user._id}*/}
-                    {/*        />*/}
-                    {/*    )))}*/}
+                   {(isPostsLoading ? [...Array(5)] : posts.items).map((obj, index) => (
+                        isPostsLoading ? (<Post key={index} isLoading={true}/>)
+                            : (
+                            <Post
+                                key={index}
+                                _id={obj._id}
+                                title={obj.title}
+                                imageUrl={obj.imageUrl ? `${process.env.REACT_APP_API_URL}${obj.imageUrl}` : ''}
+                                user={obj.user}
+                                createdAt={obj.createdAt}
+                                viewsCount={obj.viewsCount}
+                                commentsCount={3}
+                                tags={obj.tags}
+                                isEditable={userData?._id === obj.user._id}
+                            />
+                        )))}
                 </Grid>
                 <Grid xs={4} item>
                     {/*<TagsBlock items={tags?.items} isLoading={isTagsLoading}/>*/}
